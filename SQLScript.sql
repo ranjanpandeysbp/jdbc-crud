65932a48a9625551f891b133d553718d56a3f0ff
Ranjan <skillsapphire@gmail.com>
2023-02-27 09:31:11 +0100

    added jar and sql script

---
+++ SQLScript.sql
@@ -0,0 +1,18 @@
+create database foodiedb;
+
+use foodiedb;
+
+CREATE TABLE `users` (
+	`user_id` int(11) NOT NULL AUTO_INCREMENT,
+	`username` varchar(45) NOT NULL,
+	`password` varchar(45) NOT NULL,
+	`fullname` varchar(45) NOT NULL,
+	`email` varchar(45) NOT NULL,
+	PRIMARY KEY (`user_id`)
+);
+
+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
+
+git remote add origin https://github.com/ranjanpandeysbp/jdbc-crud.git
+
+git remote set-url origin https://ranjanpandeysbp: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@github.com/ranjanpandeysbp/jdbc-crud.git
